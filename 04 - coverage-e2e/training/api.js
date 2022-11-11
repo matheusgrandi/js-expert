3c793b3aa111b42f0ae1486d63e7ec3fdf0e2905
matheusgrandi <matheus@matheusgrandi.com.br>
2022-11-11 12:24:47 -0300

    :rocket: studies deployment

---
+++ 04 - coverage-e2e/training/api.js
@@ -0,0 +1,47 @@
+const http = require('http');
+
+const DEFAULT_USER = { username: 'Matheus Grandi', password: '123' };
+
+const routes = {
+  '/contact:get': (request, response) => {
+    response.write('contact us page');
+    return response.end();
+  },
+  '/login:post': async (request, response) => {
+    for await (const data of request) {
+      const user = JSON.parse(data);
+      if (
+        user.username !== DEFAULT_USER.username ||
+        user.password !== DEFAULT_USER.password
+      ) {
+        response.writeHead(401);
+        response.write('Logging failed!');
+        return response.end();
+      }
+      response.write('Login has succeeded!');
+      response.end();
+    }
+  },
+  default: (request, response) => {
+    response.write('Hello World!');
+    return response.end();
+  },
+};
+
+const handler = function (request, response) {
+  const { url, method } = request;
+  const routeKey = `${url}:${method.toLowerCase()}`;
+  console.log(routeKey);
+  const chosen = routes[routeKey] || routes.default;
+
+  response.writeHead(200, {
+    'Content-Type': 'text/html',
+  });
+  return chosen(request, response);
+};
+
+const app = http
+  .createServer(handler)
+  .listen(3031, () => console.log('API running at port 3031'));
+
+module.exports = app;
---
+++ 04 - coverage-e2e/training/api.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const { describe, it } = require('mocha');
+const app = require('./api');
+const assert = require('assert');
+
+describe('API Suite Test', () => {
+  describe('/contact', () => {
+    it('should request the contact page and return HTTP status 200', async () => {
+      const response = await request(app).get('/contact').expect(200);
+      assert.deepEqual(response.text, 'contact us page');
+    });
+  });
+  describe('/hello', () => {
+    it('should request an inexistent route /hi and redirect to /hello', async () => {
+      const response = await request(app).get('/hi').expect(200);
+      assert.deepEqual(response.text, 'Hello World!');
+    });
+  });
+  describe('/login', () => {
+    it('should be able to login with success on the login route and return status 200', async () => {
+      const response = await request(app)
+        .post('/login')
+        .send({ username: 'Matheus Grandi', password: '123' })
+        .expect(200);
+      assert.deepStrictEqual(response.text, 'Login has succeeded!');
+    });
+    it('should unauthorize a request when requesting it using wrong credentials and return status 401', async () => {
+      const response = await request(app)
+        .post('/login')
+        .send({ username: 'Aristeu', password: '312' })
+        .expect(401);
+      assert.ok(response.unauthorized);
+      assert.deepStrictEqual(response.text, 'Logging failed!');
+    });
+  });
+});
