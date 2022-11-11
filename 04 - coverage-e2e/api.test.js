f439007551a64615abd0c7fcbd11320a217479d6
matheusgrandi <matheus@matheusgrandi.com.br>
2022-11-10 22:44:25 -0300

    :rocket: covarage e2e studies deployment

--- 04 - coverage-e2e/api.test.js
+++ 04 - coverage-e2e/api.test.js
@@ -16,4 +16,21 @@ describe('API Suite Test', () => {
       assert.deepStrictEqual(response.text, 'Hello World!');
     });
   });
+  describe('/login', () => {
+    it('should be able to login with success on the login router and return status 200', async () => {
+      const response = await request(app)
+        .post('/login')
+        .send({ username: 'Matheus Grandi', password: '123' })
+        .expect(200);
+      assert.deepStrictEqual(response.text, 'Login has succeeded!');
+    });
+    it('should unauthorize a request when requesing it using wrong credentials and return status 401', async () => {
+      const response = await request(app)
+        .post('/login')
+        .send({ username: 'Nunes Filho', password: '321' })
+        .expect(401);
+      assert.ok(response.unauthorized);
+      assert.deepStrictEqual(response.text, 'Logging failed!');
+    });
+  });
 });
