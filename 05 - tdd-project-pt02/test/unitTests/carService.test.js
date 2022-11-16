9ee121de3c48112fd39712918eaacde908993be1
matheusgrandi <matheus@matheusgrandi.com.br>
2022-11-16 12:14:26 -0300

    :rocket: class 05-pt2 deployment

--- 05 - tdd-project-pt02/test/unitTests/carService.test.js
+++ 05 - tdd-project-pt02/test/unitTests/carService.test.js
@@ -1,6 +1,7 @@
-const { describe, it, before } = require('mocha');
+const { describe, it, before, beforeEach } = require('mocha');
 const { join } = require('path');
-const assert = require('assert');
+const { expect } = require('chai');
+const sinon = require('sinon');
 
 const CarService = require('./../../src/service/carService');
 
@@ -14,15 +15,53 @@ const mocks = {
 
 describe('CarService Suite Test', () => {
   let carService = {};
+  let sandbox = {};
   before(() => {
-    carService = new CarService({ cars: carsDatabase });
+    carService = new CarService({
+      cars: carsDatabase,
+    });
+  });
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+  it('should retrieve a random position from an array', () => {
+    const data = [0, 1, 2, 3, 4];
+    const result = carService.getRandomPositionFromArray(data);
+
+    expect(result).to.be.lt(data.length).and.be.gte(0);
+  });
+  it('should choose the first id from carIds in carCategory', () => {
+    const carCategory = mocks.validCarCategory;
+    const carIdIndex = 0;
+
+    sandbox
+      .stub(carService, carService.getRandomPositionFromArray.name)
+      .returns(carIdIndex);
+
+    result = carService.chooseRandomCar(carCategory);
+    const expected = carCategory.carIds[carIdIndex];
+
+    expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok;
+    expect(result).to.be.equal(expected);
   });
   it('given a carCategory it should return an available car', async () => {
     const car = mocks.validCar;
-    const carCategory = Object.create(mocks.carCategory);
-    carCategory.ids = [car.id];
+    const carCategory = Object.create(mocks.validCarCategory);
+    carCategory.carIds = [car.id];
+
+    sandbox
+      .stub(carService.carRepository, carService.carRepository.find.name)
+      .resolves(car);
+    sandbox.spy(carService, carService.chooseRandomCar.name);
+
     const result = await carService.getAvailableCar(carCategory);
     const expected = car;
-    assert.deepStrictEqual(result, expected);
+
+    expect(carService.chooseRandomCar.calledOnce).to.be.ok;
+    expect(carService.carRepository.find.calledWithExactly(car.id));
+    expect(result).to.be.deep.equal(expected);
   });
 });
