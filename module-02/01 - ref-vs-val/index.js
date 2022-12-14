aadbf95eb65c4e93d9be5ae6b5dc9aca45570f75
matheusgrandi <matheus@matheusgrandi.com.br>
2022-12-14 07:46:47 -0300

    :rocket: deploy class 3

---
+++ module-02/01 - ref-vs-val/index.js
@@ -0,0 +1,17 @@
+const { deepStrictEqual } = require('assert');
+let counter = 0;
+let counter2 = counter;
+counter2++;
+
+const item = { counter: 0 };
+const item2 = item;
+
+// tipo primitivo gera uma cópia em memória
+deepStrictEqual(counter, 0);
+deepStrictEqual(counter2, 1);
+
+// tipo de referência copia o endereço de memória e aponta para o mesmo lugar
+item.counter++;
+deepStrictEqual(item, { counter: 1 });
+item.counter++;
+deepStrictEqual(item2, { counter: 2 });
