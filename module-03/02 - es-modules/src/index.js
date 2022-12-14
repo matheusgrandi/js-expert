775d8daced0d65bda4846960b1242b359ca38502
matheusgrandi <matheus@matheusgrandi.com.br>
2022-12-14 15:25:55 -0300

    :construction: class 02 progress

--- module-03/02 - es-modules/src/index.js
+++ module-03/02 - es-modules/src/index.js
@@ -1,37 +1,27 @@
-import draftLog from 'draftlog';
-import chalk from 'chalk';
-import chalkTable from 'chalk-table';
-import readline from 'readline';
-
+import TerminalController from './terminalController.js';
 import database from './../database.json' assert { type: 'json' };
 import Person from './person.js';
 
 const DEFAULT_LANG = 'pt-br';
+const STOP_TERM = ':q';
 
-draftLog(console).addLineListener(process.stdin);
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
 
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: 'id', name: chalk.cyan('ID') },
-    { field: 'vehicles', name: chalk.magenta('Vehicles') },
-    { field: 'kmTraveled', name: chalk.cyan('Km traveled') },
-    { field: 'from', name: chalk.cyan('From') },
-    { field: 'to', name: chalk.cyan('To') },
-  ],
-};
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question('What?? ');
+    if (answer === STOP_TERM) {
+      terminalController.closeTerminal();
+      console.log('process finished!');
+      return;
+    }
+    const person = Person.generateInstanceFromString(answer);
+    console.log('person', person.formatted(DEFAULT_LANG));
+    return mainLoop();
+  } catch (error) {
+    console.error('Something wrong occurred');
+  }
+}
 
-const table = chalkTable(
-  options,
-  database.map((item) => new Person(item).formatted(DEFAULT_LANG))
-);
-const print = console.draft(table);
-
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-terminal.question('Qual é o seu nome? ', (msg) => {
-  console.log('msg', msg.toString());
-});
+await mainLoop();
--- module-03/02 - es-modules/src/person.js
+++ module-03/02 - es-modules/src/person.js
@@ -1,13 +1,18 @@
 export default class Person {
-  constructor({ id, vehicles, kmTraveled, form, to }) {
+  constructor({ id, vehicles, kmTraveled, from, to }) {
     this.id = id;
     this.vehicles = vehicles;
     this.kmTraveled = kmTraveled;
-    this.form = form;
+    this.from = from;
     this.to = to;
   }
 
   formatted(language) {
+    const mapDate = (date) => {
+      const [year, month, day] = date.split('-').map(Number);
+      return new Date(year, month - 1, day);
+    };
+
     return {
       id: Number(this.id),
       vehicles: new Intl.ListFormat(language, {
@@ -22,12 +27,26 @@ export default class Person {
         month: 'long',
         day: '2-digit',
         year: 'numeric',
-      }).format(this.from),
+      }).format(mapDate(this.from)),
       to: new Intl.DateTimeFormat(language, {
         month: 'long',
         day: '2-digit',
         year: 'numeric',
-      }).format(this.from),
+      }).format(mapDate(this.to)),
     };
+  }
+
+  static generateInstanceFromString(text) {
+    const EMPTY_SPACE = ' ';
+    const [id, vehicles, kmTraveled, from, to] = text.split(EMPTY_SPACE);
+    const person = new Person({
+      id,
+      vehicles: vehicles.split(','),
+      kmTraveled,
+      from,
+      to,
+    });
+
+    return person;
   }
 }
